6f482cb821217bba5397d2a323839a741a921443
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2024-04-17 21:34:36 +0600

    Add files via upload

---
+++ Register program.py
@@ -0,0 +1,8 @@
+name = input("Hi, what is your name?")
+age = int(input("How old are you?"))
+
+if (age < 13):
+    print(" You are too young to register",  name)
+else:
+    print("Feel free to join", name)
+    
