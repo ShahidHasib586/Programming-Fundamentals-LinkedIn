ea8b24f3e87b747d7da8c779058032691a8d3a71
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2024-09-27 04:39:25 +0600

    Add files via upload

---
+++ hypotenious of the right angle triangle.py
@@ -0,0 +1,8 @@
+from math import * 
+
+a = float(input("Enter the base of the right angle triangle: \n"))
+b = float (input("Enter the height of the right angle: \n"))
+
+h = sqrt(pow(a,2)+pow(b,2))
+
+print(f"The hypotenious of the right angle triangle is: \n {h} cm")
