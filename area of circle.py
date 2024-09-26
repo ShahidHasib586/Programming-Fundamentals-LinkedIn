68215e93a801a3ec1dbe993ea2a97e587a001617
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2024-09-27 04:32:20 +0600

    Add files via upload

--- area of circle.py
+++ area of circle.py
@@ -3,3 +3,7 @@ from math import *
 r = float(input("Enter the Radius of the Circle: \n"))
 
 print("The area of the circle is: ", pi*(r**2),"cm^2")
+
+print("The area of the circle is: ", pi*(pow(r, 2)),"cm^2")
+
+print("The area of the circle is: ", round(pi*(pow(r, 2)),3),"cm^2")
