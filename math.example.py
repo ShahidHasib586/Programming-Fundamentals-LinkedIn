a8bb551a8814105da8ad65d26126b281346e27ef
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2024-09-27 03:30:09 +0600

    Add files via upload

---
+++ math.example.py
@@ -0,0 +1,8 @@
+#import math
+from math import *
+
+r = float(input("Enter the Radius of the Circle: \n"))
+#pi = math.pi
+
+print("The radius of the circle is: ", 2*r*pi,"cm")
+print("The radius of the circle rounded in 3 decimal points is: ", round(2*r*pi, 3),"cm")
