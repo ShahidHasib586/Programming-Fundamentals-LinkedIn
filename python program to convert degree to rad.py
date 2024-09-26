c3e627d9bb9a25cc97b1086ee8ab22d3b21f33c9
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2024-09-27 04:50:53 +0600

    Add files via upload

---
+++ python program to convert degree to rad.py
@@ -0,0 +1,6 @@
+#python program to convert degree to radians
+import math
+a = float (input ("Enter the angle in degree \n"))
+
+radians = a*(math.pi/180)
+print(f"The angle converted into rdians is: {radians} radians")
